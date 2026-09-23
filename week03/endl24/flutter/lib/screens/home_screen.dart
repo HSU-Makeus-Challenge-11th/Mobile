88a942afca58b4193ec327e7ff88a5c026f5d4c0
endl24 <rkdendl22@gmail.com>
2026-09-23 14:36:39 +0900

    feat: 홈 화면 인사 문구와 히어로 카드 구현

--- week03/endl24/flutter/lib/screens/home_screen.dart
+++ week03/endl24/flutter/lib/screens/home_screen.dart
@@ -1,23 +1,30 @@
-// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:movielog/models/movie.dart';
-import 'package:movielog/widgets/movie_card.dart';
+import 'package:movielog/widgets/hero_movie_card.dart';
+import 'package:movielog/widgets/home_greeting.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final movie = mockMovies.first;
+    final heroMovie = mockMovies.first;
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(16),
-          child: MovieCard(
-            movie: movie,
-            onTap: () => context.push('/movies/${movie.id}'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const HomeGreeting(),
+              const SizedBox(height: 16),
+              HeroMovieCard(
+                movie: heroMovie,
+                onTap: () => context.push('/movies/${heroMovie.id}'),
+              ),
+            ],
           ),
         ),
       ),
