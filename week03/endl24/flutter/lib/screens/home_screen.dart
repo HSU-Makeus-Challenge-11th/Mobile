840737fe24f17497f4d9ccacbb4fc22f8efe90ab
endl24 <rkdendl22@gmail.com>
2026-09-23 12:07:27 +0900

    feat: 홈 화면 영화 카드와 상세 이동 연결

--- week03/endl24/flutter/lib/screens/home_screen.dart
+++ week03/endl24/flutter/lib/screens/home_screen.dart
@@ -1,10 +1,26 @@
+// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movielog/models/movie.dart';
+import 'package:movielog/widgets/movie_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home')));
+    final movie = mockMovies.first;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: MovieCard(
+            movie: movie,
+            onTap: () => context.push('/movies/${movie.id}'),
+          ),
+        ),
+      ),
+    );
   }
 }
